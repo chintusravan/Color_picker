9ef63ec16e90dff1b0af9e1c9fe6dc6a6ac20a81
Shravankuamr <158179326+chintusravan@users.noreply.github.com>
2024-09-16 00:39:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,207 @@
+function myred(){
+    document.getElementById('text').style.backgroundColor='red';
+}
+function myyellow(){
+    document.getElementById('text').style.backgroundColor='yellow';
+
+}
+function myblue(){
+    document.getElementById('text').style.backgroundColor='blue';
+
+}
+function mydarkblue(){
+    document.getElementById('text').style.backgroundColor='darkblue';
+}
+function myaliceblue(){
+    document.getElementById('text').style.backgroundColor='aliceblue';
+}
+function myaquamarine(){
+    document.getElementById('text').style.backgroundColor='aquamarine';
+}
+function myIndianRed(){
+    document.getElementById('text').style.backgroundColor='IndianRed';
+}
+function myDarkSalmon(){
+    document.getElementById('text').style.backgroundColor='DarkSalmon';
+}
+function myCrimson(){
+    document.getElementById('text').style.backgroundColor='Crimson';
+}
+function myHotPink(){
+    document.getElementById('text').style.backgroundColor='HotPink';
+}
+function myMediumVioletRed(){
+    document.getElementById('text').style.backgroundColor='MediumVioletRed';
+}
+function myDarkOrange(){
+    document.getElementById('text').style.backgroundColor='DarkOrange';
+}
+function myGold(){
+    document.getElementById('text').style.backgroundColor='Gold';
+}
+function myMagenta(){
+    document.getElementById('text').style.backgroundColor='Magenta';
+}
+function myDarkMagenta(){
+    document.getElementById('text').style.backgroundColor='DarkMagenta';
+}
+function myLime(){
+    document.getElementById('text').style.backgroundColor='Lime';
+}
+function myOlive(){
+    document.getElementById('text').style.backgroundColor='Olive';
+}
+function myDarkCyan(){
+    document.getElementById('text').style.backgroundColor='DarkCyan';
+}
+function myAqua(){
+    document.getElementById('text').style.backgroundColor='Aqua';
+}
+function myWheat(){
+    document.getElementById('text').style.backgroundColor='Wheat';
+}
+function myBrown(){
+    document.getElementById('text').style.backgroundColor='Brown';
+}
+function myWheat(){
+    document.getElementById('text').style.backgroundColor='Wheat';
+}
+function myGhostWhite(){
+    document.getElementById('text').style.backgroundColor='GhostWhite';
+}
+function mySilver(){
+    document.getElementById('text').style.backgroundColor='Silver';
+}
+function myGhostWhite(){
+    document.getElementById('text').style.backgroundColor='GhostWhite';
+}
+function myorchid(){
+    document.getElementById('text').style.backgroundColor='orchid';
+}
+function myperu(){
+    document.getElementById('text').style.backgroundColor='peru';
+}
+function mytomato(){
+    document.getElementById('text').style.backgroundColor='tomato';
+}
+function myteal(){
+    document.getElementById('text').style.backgroundColor='teal';
+}
+function mynavyblue(){
+    document.getElementById('text').style.backgroundColor='rgb(150, 150, 226)';
+}
+function mythistle(){
+    document.getElementById('text').style.backgroundColor='thistle';
+}
+function myseagreen(){
+    document.getElementById('text').style.backgroundColor='seagreen';
+}
+function mydeeppink(){
+    document.getElementById('text').style.backgroundColor='deeppink';
+}
+
+
+
+
+function myredtext(){
+    document.getElementById('text').style.color='red';
+}
+
+function myyellowtext(){
+    document.getElementById('text').style.color='yellow';
+}
+function mybluetext(){
+    document.getElementById('text').style.color='blue';
+
+}
+function mydarkbluetext(){
+    document.getElementById('text').style.color='darkblue';
+}
+function myalicebluetext(){
+    document.getElementById('text').style.color='aliceblue';
+}
+function myaquamarinetext(){
+    document.getElementById('text').style.color='aquamarine';
+}
+function myIndianRedtext(){
+    document.getElementById('text').style.color='IndianRed';
+}
+function myDarkSalmontext(){
+    document.getElementById('text').style.color='DarkSalmon';
+}
+function myCrimsontext(){
+    document.getElementById('text').style.color='Crimson';
+}
+function myHotPinktext(){
+    document.getElementById('text').style.color='HotPink';
+}
+function myMediumVioletRedtext(){
+    document.getElementById('text').style.color='MediumVioletRed';
+}
+function myDarkOrangetext(){
+    document.getElementById('text').style.color='DarkOrange';
+}
+function myGoldtext(){
+    document.getElementById('text').style.color='Gold';
+}
+function myMagentatext(){
+    document.getElementById('text').style.color='Magenta';
+}
+function myDarkMagentatext(){
+    document.getElementById('text').style.color='DarkMagenta';
+}
+function myLimetext(){
+    document.getElementById('text').style.color='Lime';
+}
+function myOlivetext(){
+    document.getElementById('text').style.color='Olive';
+}
+function myDarkCyantext(){
+    document.getElementById('text').style.color='DarkCyan';
+}
+function myAquatext(){
+    document.getElementById('text').style.color='Aqua';
+}
+function myWheattext(){
+    document.getElementById('text').style.color='Wheat';
+}
+function myBrowntext(){
+    document.getElementById('text').style.Color='Brown';
+}
+function myWheattext(){
+    document.getElementById('text').style.color='Wheat';
+}
+function myGhostWhitetext(){
+    document.getElementById('text').style.color='GhostWhite';
+}
+function mySilvertext(){
+    document.getElementById('text').style.color='Silver';
+}
+function myGhostWhite(){
+    document.getElementById('text').style.color='GhostWhite';
+}
+function myorchidtext(){
+    document.getElementById('text').style.color='orchid';
+}
+function myperutext(){
+    document.getElementById('text').style.color='peru';
+}
+function mytomatotext(){
+    document.getElementById('text').style.color='tomato';
+}
+function mytealtext(){
+    document.getElementById('text').style.color='teal';
+}
+function mynavybluetext(){
+    document.getElementById('text').style.color='rgb(150, 150, 226)';
+}
+function mythistletext(){
+    document.getElementById('text').style.color='thistle';
+}
+function myseagreentext(){
+    document.getElementById('text').style.color='seagreen';
+}
+function mydeeppinktext(){
+    document.getElementById('text').style.color='deeppink';
+}
+
